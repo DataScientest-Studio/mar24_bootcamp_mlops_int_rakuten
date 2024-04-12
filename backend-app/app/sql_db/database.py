2e3a3810ec5c2ab62b4db1263aadd03a2c20d2e9
rudolf b. <braun.ru@outlook.de>
2024-04-12 08:47:31 +0200

    Change to postresql db

--- backend-app/app/sql_db/database.py
+++ backend-app/app/sql_db/database.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import 
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@localhost:5432/rakuten_db" #BUG: Change to .env  #"sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # WARNING:'check_same_thread only sqllite remove late
+    SQLALCHEMY_DATABASE_URL #, connect_args={"check_same_thread": False}  #'check_same_thread only sqllite remove late
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
